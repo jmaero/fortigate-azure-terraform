5ae2c7d422a68fa7736e0d34963ed89c8b9169b1
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-10-25 23:09:12 -0400

    adding pre-commit

--- init.sh
+++ init.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #
 
-curl -O https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_amd64.zip
+if [[ "${AZUREPS_HOST_ENVIRONMENT}" =~ ^cloud-shell.* ]]; then
+	if [[ ! -d ~/.local/bin ]]; then
+		mkdir -p ~/.local/bin
+	fi
+	wget -q https://github.com/cli/cli/releases/download/v2.35.0/gh_2.35.0_linux_amd64.tar.gz -O ~/gh.tar.gz
+	tar --strip-components=2 -C ~/.local/bin/ -zxf ~/gh.tar.gz gh_2.35.0_linux_amd64/bin/gh
+	wget -q https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_amd64.zip -O ~/terraform.zip
+	unzip ~/terraform.zip
+	chmod 755 ~/terraform && mv ~/terraform ~/.local/bin/
+fi
