a67a0963bd59bca8487297798d7fff073ec80761
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-10-26 03:18:30 +0000

    updateing tools

--- init.sh
+++ init.sh
@@ -8,5 +8,5 @@ if [[ "${AZUREPS_HOST_ENVIRONMENT}" =~ ^cloud-shell.* ]]; then
 	wget -q https://github.com/cli/cli/releases/download/v2.35.0/gh_2.35.0_linux_amd64.tar.gz -O ~/gh.tar.gz
 	tar --strip-components=2 -C ~/.local/bin/ -zxf ~/gh.tar.gz gh_2.35.0_linux_amd64/bin/gh
 	wget -q https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_amd64.zip -O ~/terraform.zip
-	unzip ~/terraform.zip -d ~/.local/bin && chmod ~/.local/bin/terraform
+	unzip ~/terraform.zip -d ~/.local/bin && chmod 755 ~/.local/bin/terraform
 fi
